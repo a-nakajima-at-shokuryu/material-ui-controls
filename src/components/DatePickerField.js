bcf567cf050c813842a78c8403c28b04e28fc539
a-nakajima <a-nakajia@shokuryu.co.jp>
2020-02-06 15:23:37 +0900

    refactor DatePickerField

--- src/components/DatePickerField.js
+++ src/components/DatePickerField.js
@@ -1,109 +1,101 @@
-import React from 'react';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'; 
-import DateFnsUtils from '@date-io/date-fns'; 
-import ja from 'date-fns/locale/ja'; 
-import { useTheme, MuiThemeProvider, createMuiTheme } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
+import React, { useEffect, useMemo } from 'react';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import ja from 'date-fns/locale/ja';
 import { getDay, isSameDay } from 'date-fns';
+import { MuiThemeProvider, createMuiTheme, useTheme } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
 
-const useUtilsWrapper = (weekStartsOn) => {
-  ja.options.weekStartsOn = weekStartsOn;
-
-  return children => (
+const useUtilsProvider = (weekStartsOn) => {
+  useEffect(() => {
+    ja.options.weekStartsOn = weekStartsOn; 
+  }, [weekStartsOn]);
+  return element => (
     <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ja}>
-      {children}
+      {element}
     </MuiPickersUtilsProvider>
   );
 };
 
-const useThemeWrapper = (theme) => {
-  return children => (
+const useThemeProvider = theme => {
+  return element => (
     <MuiThemeProvider theme={theme}>
-      {children}
+      {element}
     </MuiThemeProvider>
   );
 };
 
-const useCreateTheme = (weekStartsOn, date) => {
+const useCreateTheme = (dayOfWeek, weekStartsOn) => {
   const theme = useTheme();
   const primaryMain = blue[700];
   const sundayColor = theme.palette.secondary.main;
   const saturdayColor = theme.palette.success.dark; 
-  const switchWeekColor = date => {
-    switch (getDay(date)) {
-      case 0: return sundayColor; 
-      case 6: return saturdayColor; 
-      default: return undefined; 
-    }
-  };
-  const weekColor = switchWeekColor(date) || primaryMain; 
-
+  const getWeekColor = dayOfWeek => (
+    dayOfWeek === 0 ? sundayColor : (
+    dayOfWeek === 6 ? saturdayColor: (
+      undefined
+  )));
+  const weekColor = getWeekColor(dayOfWeek);
   const sundayPos = (0 - weekStartsOn + 7) % 7;
   const saturdayPos = (6 - weekStartsOn + 7) % 7;
-
-
   const createTheme = () => createMuiTheme({
     palette: {
       primary: {
-        main: weekColor, 
+        main: weekColor || primaryMain, 
       }, 
     }, 
     overrides: {
-      MuiPickersCalendarHeader: {
-        dayLabel: {
-          [`&:nth-child(${sundayPos + 1})`]: {
-            color: sundayColor, 
-          }, 
-          [`&:nth-child(${saturdayPos + 1})`]: {
-            color: saturdayColor, 
-          }, 
-        }, 
-      }, 
       MuiPickersDay: {
         current: {
-          color: primaryMain, 
+          color: primaryMain
+        }, 
+      }, 
+      MuiPickersCalendarHeader: {
+        dayLabel: {
+          [`&:nth-child(${sundayPos + 1})`]: { color: sundayColor }, 
+          [`&:nth-child(${saturdayPos + 1})`]: { color: saturdayColor }, 
         }, 
       }, 
     }, 
   });
-
   const renderDay = (day, selectedDate, dayInCurrentMonth, dayComponent) => {
     const selected = isSameDay(day, selectedDate);
-    const weekColor = selected ? undefined : switchWeekColor(day);
+    const dayColor = selected ? undefined : getWeekColor(getDay(day));
     return React.cloneElement(dayComponent, {
       style: {
-        color: weekColor, 
-      },
+        color: dayColor,
+      },  
     });
   };
-
   return [createTheme, renderDay];
 };
-
 const DatePickerField = ({
   value, 
   onChange, 
+  label="納品日", 
   weekStartsOn = 0, 
   ...other
 }) => {
-  const [createTheme, renderDay] = useCreateTheme(weekStartsOn);
-  const utilsWrapper = useUtilsWrapper(weekStartsOn);
-  const themeWrapper = useThemeWrapper(createTheme());
-  const wrapper = c => [
-    utilsWrapper, 
-    themeWrapper, 
-  ].reduce((c, w) => w(c), c);
-
-  return wrapper(
-    <KeyboardDatePicker 
+  const dayOfWeek = useMemo(() => getDay(value), [value]);
+  const [createTheme, renderDay] = useCreateTheme(dayOfWeek, weekStartsOn);
+  const themeProvider = useThemeProvider(createTheme());
+  const utilsProvider = useUtilsProvider(weekStartsOn)
+  const all = (...a) => e => a.reduce((e, f) => f(e), e);
+  const provider = all(
+    themeProvider, 
+    utilsProvider, 
+  );
+  return provider(
+    <KeyboardDatePicker
       variant="inline"
       format="yyyy-MM-dd"
-      autoOk
+      autoOk 
 
       renderDay={renderDay}
 
       value={value}
       onChange={onChange}
+      label={label}
       {...other}
     />
   )
